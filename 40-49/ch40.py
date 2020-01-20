59afc106ca4b188f4fe5f0fbc1caf560e31fa69d
Antoxyde <Antoxyde@protonmail.com>
2020-01-20 07:37:43 +0100

    ajout ch40

---
+++ 40-49/ch40.py
@@ -0,0 +1,9 @@
+d = "0"
+
+i = 1
+while len(d) < 1000001:
+    d += str(i)
+    i += 1
+
+print(int(d[1]) * int(d[10]) * int(d[100]) * int(d[1000]) * int(d[10000]) * int(d[100000]) * int(d[1000000]))
+
